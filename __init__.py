2afddc47ae6d54a9f5d9a98119bf09cbc942e001
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-04-30 00:40:38 +0200

    * Updtae Todo

--- __init__.py
+++ __init__.py
@@ -13,6 +13,8 @@ bl_info = {
 	"category" : "Import-Export"
 }
 
+# Todo : 
+#	- Need to check the shared Data before importing, and relink and clean if shared
 class TILA_OP_ExportAsBlend(bpy.types.Operator, ImportHelper):
 	bl_idname = "export_scene.tila_blend"
 	bl_label = "Export as Blend"
