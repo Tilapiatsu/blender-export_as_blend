76a8285ba8bed4692ec429206a46ff9b280824d2
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-05-06 13:31:12 +0200

    * Update Version Number

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ bl_info = {
 	"name": "Export as Blend",
 	"author": "Tilapiatsu",
 	"description": "This addon allow you to export data to a new blend file, from selected Objects or a comlplete Scene.",
-	"version": (1, 0, 3),
+	"version": (1, 1, 0),
 	"blender": (3, 1, 0),
 	"location": "File > Export > Export as Blend (.blend)",
 	"warning": "",
